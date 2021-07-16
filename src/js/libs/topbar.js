21b2f545c0b61897bb0bde0fca3fef6b83621274
Miguel Martinez <miguel.martinez@MC-004394-AP.local>
2021-07-16 12:26:01 -0500

    scroll topbar

--- src/js/libs/topbar.js
+++ src/js/libs/topbar.js
@@ -1,16 +1,18 @@
 var topbar = function () {
-
 	var header = $('.Header'),
 			previousScroll = 0,
     	originalTop = header.offset().top;
-
+			console.log(previousScroll, originalTop);
   $(window).scroll(
   	function(e){
-  		if ($(this).scrollTop() >= originalTop + 50) {
+			var currentScroll = $(this).scrollTop();
+  		if (currentScroll >= originalTop+400) {
   			header.addClass('is-scrolled');
+				console.log('hola amigo');
   		}
 	    else {
 	    	header.removeClass('is-scrolled');
+				console.log('adios amigo');
 	    }
 	});
 }
